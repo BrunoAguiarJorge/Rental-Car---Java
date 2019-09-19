48b6dd452f3ae0db7e8824739358bc0480db38e1
Bruno Aguiar <brunoaguiar@Brunos-MacBook-Air.local>
2019-09-19 17:28:13 +0100

    implementing the service layer

---
+++ src/model/services/BrazilTaxService.java
@@ -0,0 +1,14 @@
+package model.services;
+
+public class BrazilTaxService {
+	
+	//Double with capital D is type class and double with small d is type of primitivo.
+	public double tax(double amount) {
+		if (amount <= 100.0) {
+			return amount * 0.2;
+		}
+		else {
+			return amount * 0.15;
+		}
+	}
+}
